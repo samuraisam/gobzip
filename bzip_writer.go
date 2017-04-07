4abfc3fba744a46217e8b40d9d7843237e73c3ea
Samuel Sutch <sam@sutch.net>
2017-04-07 12:10:11 -0700

    move tempFile into main lib file

--- bzip_writer.go
+++ bzip_writer.go
@@ -12,6 +12,10 @@ import (
 	"io"
 	"os"
 	"unsafe"
+	"hash/fnv"
+	"path/filepath"
+
+	"github.com/satori/go.uuid"
 )
 
 // this is a go binding to bzip2 compressor
@@ -171,4 +175,11 @@ func BzipError(err int) error {
 		return ErrOutbufFull
 	}
 	return ErrUnknown
+}
+
+// use uuid to generate file name
+func tempFile(dir, prefix string) string {
+	fnv1a := fnv.New64a()
+	fnv1a.Write(uuid.NewV1().Bytes())
+	return filepath.Join(fmt.Sprintf("%s", dir), fmt.Sprintf("%s-%d", prefix, uint64(fnv1a.Sum64())))
 }
